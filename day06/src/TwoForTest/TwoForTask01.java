1c2b30d311be7364ba588531ee041df2d0bfd3b4
rohhyoungwoo <nungma97@naver.com>
2024-11-28 17:49:52 +0900

    day06파일저장

---
+++ day06/src/TwoForTest/TwoForTask01.java
@@ -0,0 +1,48 @@
+package TwoForTest;
+
+public class TwoForTask01 {
+	public static void main(String[] args) {
+//		i, j의 규칙성 찾기
+		
+		
+//		1번	
+//		*****
+//		****
+//		***
+//		**
+//		*
+		for(int i =1; i <= 5; i++) {
+			for(int j =5; j <= 1; j--) {
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}
+		
+		
+		
+//		2번 : 피라미드와 유사한
+//		*
+//		* * *
+//		* * * * *
+//		* * * * * * *
+		
+//		3번 : 피라미드 (for문이 총 3개-> 어떻게 사용될지 먼저 생각하기)
+//		 *
+//		* *
+//	   * * *
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
