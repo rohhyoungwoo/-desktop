39734e8acdb336bff8a083848a951afa4953ecfb
rohhyoungwoo <nungma97@naver.com>
2024-12-06 18:00:40 +0900

    241206 파일저장

--- day10/src/task01/animalsMain.java
+++ day10/src/task01/animalsMain.java
@@ -4,7 +4,10 @@ public class animalsMain {
 	public static void main(String[] args) {
 		// tiger, rabbit, Lion
 		animals tiger = new animals("어흥이", 97 ,"호랑이");
-		tiger.move();
+		tiger.eat();
+		tiger.playing();
+		tiger.sleep();
+		tiger.birthday();
 		
 		
 		
